f3d2257fd9a14dc87e78eae52e88122f7e8742ed
kadija-ssd <kadija.sid@outlook.fr>
2023-04-06 12:36:56 +0200

    modification

--- front/js/produits.js
+++ front/js/produits.js
@@ -1,208 +1,121 @@
-// récupère l'URL et l'ID de l'article
+let url = new URL(location.href); //déclare une variable valant l'url de la page actuelle
+let kanapPageId = url.searchParams.get("id"); //récupère l'id contenu dans l'url de la page actuelle
 
-let urlId= new URLSearchParams(window.location.search).get('id');
-ProductPage();
+const zoneImgKanap = document.querySelector(".item__img");
+const nomKanap = document.querySelector("#title");
+const prixKanap = document.querySelector("#price");			// emplacements des différentes zones
+const speechKanap = document.querySelector("#description");	// d'insertion des variables dynamiques
+const colorOptions = document.querySelector("#colors");
+let getProductQuantity = document.querySelector("#quantity");
 
-// fonction qui repère un produit grace à son id et qui charge une fonction
+fetch(`http://localhost:3000/api/products/${kanapPageId}`) //je ne selectionne QUE la partie du JSON qui m'interesse en fonction de l'id du kanap concerné à fetch
+	.then((res) => res.json())
+	.then((object) => {
+		const imgKanap = object.imageUrl;
+		const nameKanap = object.name;
+		const priceKanap = object.price;
+		const descriptKanap = object.description;
+		const colorsKanap = object.colors;
 
-async function ProductPage() {
-	await fetch('http://localhost:3000/api/products/' + urlId)
-		.then((res) => res.json())
-		.then((data) => page(data))
-		.catch((error) => {
-			console.log(error);
-			window.alert('Connexion au serveur impossible !');
-		});
-}
-
-// fonction appelé directement après récupération des données du fetch
-
-function page(data) {
-	if (data != null) {
-		let ficheProduct = document.querySelector('.item__img');
-		ficheProduct.innerHTML += `<img src="${data.imageUrl}" alt="${data.altTxt}">`;
-
-		ficheProduct = document.querySelector('#title');
-		ficheProduct.textContent = data.name;
-
-		ficheProduct = document.querySelector('#price');
-		ficheProduct.textContent = data.price;
-
-		ficheProduct = document.querySelector('#description');
-		ficheProduct.textContent = data.description;
-
-		for (let i = 0; i < data.colors.length; i++) {
-			canapColors(data.colors[i]);
+		for (let couleur of colorsKanap) {
+			colorOptions.innerHTML += `<option value="${couleur}">${couleur}</option>`;
 		}
-	}
-}
-// creation du choix de couleur du canapé
-function canapColors(varChoix) {
-	const varOption = document.createElement('option');
-	varOption.value = varChoix;
-	varOption.textContent = varChoix;
-	const ficheProduct = document.querySelector('#colors');
-	ficheProduct.appendChild(varOption);
-}
+		zoneImgKanap.innerHTML += `<img src="${imgKanap}" alt="Photographie d'un canapé">`;
+		nomKanap.innerText += `${nameKanap}`;
+		prixKanap.innerText += `${priceKanap} `;
+		speechKanap.innerText += `${descriptKanap}`;
 
-// function qui contrôle la quantité tapé directement
+		// je crée une fonction déclenchée au clic sur le bouton ADDTOCART
 
-function controlQuantity() {
-	const quantity = document.querySelector('#quantity').value;
-	if (quantity != null) {
-		if (quantity < 0) document.querySelector('#quantity').value = 0;
-		if (quantity > 100) document.querySelector('#quantity').value = 100;
-	}
-}
+		const button = document.getElementById("addToCart");
 
+		//---------------------------------------localStorage----------------------------------------------------
 
-// déclenche la fonction addQuantityToCard au click sur le bouton
-document.querySelector('#addToCart').addEventListener('click', addQuantityToCart);
-//
-//-----------------------------------------------------
-// déclenche la fonction modifyQuantity lorsque la quantité est modifié, et la met à jour
-//-----------------------------------------------------
-//
-document.querySelector('[name="itemQuantity"]').addEventListener('input', modifyQuantity);
-document.querySelector('[name="itemQuantity"]').addEventListener('keyup', controlQuantity);
+		// liste des actions déclenchées au clic sur le bouton "ajouter"
+		button.addEventListener("click", () => {
+			let basketValue = {
+				//initialisation de la variable basketValue
+				idSelectedProduct: kanapPageId,
+				nameSelectedProduct: nameKanap,
+				colorSelectedProduct: colorOptions.value,
+				quantity: getProductQuantity.value
+			};
 
-// ajout au panier 
-
-// function qui ajoute un article et test s'il existe déjà, si oui, alors il ajoute la quantité à l'article existant
-//-----------------------------------------------------
-//
-function addQuantityToCart() {
-	// récupère la quantité et la couleur sélectionné
-	const newQuantity = document.querySelector('#quantity').value;
-	const currentColor = document.querySelector('#colors').value;
-
-	if (newQuantity > 0 && newQuantity <= 100 && currentColor != '') {
-		let arrayProduct = JSON.parse(localStorage.getItem('product'));
-		colorGrisBorder();
-		let objJson = {
-			id: urlId,
-			quantity: parseInt(newQuantity),
-			color: currentColor,
-		};
-		if (arrayProduct == null) {
-			arrayProduct = [];
-			arrayProduct.push(objJson);
-		} else {
-			const productSearch = arrayProduct.find((product) => product.id == objJson.id && product.color == objJson.color);
-			if (productSearch != undefined) {
-				const valeurActuelle = productSearch.quantity;
-				const addValue = parseInt(valeurActuelle) + parseInt(newQuantity);
-				if (addValue > 100) {
-					arrayProduct.quantity = 100;
-					if (valeurActuelle > 100) {
-						alert(`vous avez déjà ${valeurActuelle} article(s) dans votre panier, 
-                        il est limité à 100 articles ! 
-                        `);
-						document.getElementById("addToCart").disabled = true;
-					} else {
-						alert('Le panier à atteint sa limite maximale');
-						document.getElementById("addToCart").disabled = true;
-					}
+			//je crée une fonction de récupération du panier
+			function getBasket() {
+				let basketValue = JSON.parse(localStorage.getItem("kanapLs"));
+				if (basketValue === null) {
+					return [];				//si le LocalStorage est vide, on crée un tableau vide
 				} else {
-					arrayProduct.quantity = addValue;
-				}
-				arrayProduct.forEach((product) => {
-					if (product.id == objJson.id && product.color == objJson.color) {
-						if (addValue <= 100) {
-							product.quantity = parseInt(product.quantity) + parseInt(objJson.quantity);
-						} else {
-							product.quantity = 100;
-						}
-					}
-				});
-			} else {
-				const productSearch = arrayProduct.find((product) => product.id == objJson.id && product.color == objJson.color);
-				let valeurActuelle = 0;
-				if(productSearch.quantity != undefined) {
-					valeurActuelle=productSearch.quantity;
-				}
-				const addValue = parseInt(valeurActuelle) + parseInt(newQuantity);
-				if (addValue > 100) {
-					alert(`vous avez déjà ${valeurActuelle} article(s) dans votre panier, 
-					il est limité à 100 articles ! 
-					${max} articles sont ajouté au panier !`);
-					document.getElementById("addToCart").disabled = true;
-				} else {
-					arrayProduct.push(objJson);
+					return basketValue
 				}
 			}
-		}
-		localStorage.setItem('product', JSON.stringify(arrayProduct));
-		window.location.href = 'index.html';
-	} else {
-		// test le/les champs qui n'ont pas été renseigné.
-		testContentFields(newQuantity, currentColor);
-	}
-}
-//
-//-----------------------------------------------------
-// fonction qui récupère la quantité MAJ et l'enregistre dans le localStorage
-//-----------------------------------------------------
-//
-function modifyQuantity() {
-	const currentColor = parseInt(document.querySelector('#colors').value);
-	let arrayProduct = findIdColor(urlId, currentColor);
-	let currentQuantity = parseInt(document.querySelector('#quantity').value);
-	if (currentQuantity != null && arrayProduct != undefined) {
-		arrayProduct.quantity = currentQuantity;
-		localStorage.setItem('product', JSON.stringify(arrayProduct));
-	}
-}
-//
-//-----------------------------------------------------
-// fonction qui recherche un doublon ou les paramètres "id" et "color" sont ceux de l'article en cours
-//-----------------------------------------------------
-//
-function findIdColor(id, color) {
-	let item = {};
-	for (let i = 0; i < localStorage.length; i++) {
-		item = JSON.parse(localStorage.getItem('product', i));
-		if (item.id == id && item.color == color) return item;
-	}
-	return undefined;
-}
-//
-//-----------------------------------------------------
-// fonction qui test si les champs sont remplis, sinon change les bordures en rouge
-//-----------------------------------------------------
-//
-function testContentFields(varQuantity, varColor) {
-	if (varQuantity <= 0 || varQuantity > 100) {
-		const varElement = document.querySelector('input');
-		const varParent = document.getElementById('#quantity');
-		varElement.setAttribute('style', 'border:2px solid #FF0000;');
-	} else {
-		const varSelect = document.querySelector('input');
-		const varColors = document.getElementById('#quantity');
-		varSelect.setAttribute('style', 'border:1px solid #767676;');
-	}
-	if (varColor == '') {
-		const varElement = document.querySelector('select');
-		const varParent = document.getElementById('#colors');
-		varElement.setAttribute('style', 'border:2px solid #FF0000;');
-	} else {
-		const varElement = document.querySelector('select');
-		const varParent = document.getElementById('#colors');
-		varElement.setAttribute('style', 'border:1px solid #767676;');
-	}
-}
-//
-//-----------------------------------------------------
-// fonction qui met les bordures en gris
-//-----------------------------------------------------
-//
-function colorGrisBorder() {
-	const varInput = document.querySelector('input');
-	const varQuantity = document.getElementById('#quantity');
-	varInput.setAttribute('style', 'border:1px solid #767676;');
 
-	const varSelect = document.querySelector('select');
-	const varColors = document.getElementById('#colors');
-	varSelect.setAttribute('style', 'border:1px solid #767676;');
-}
+			//je crée une fonction d'ajout au panier avec argument product
+			function addBasket(product) {
+				let basketValue = getBasket();
+				let foundProducts = basketValue.find(
+					/// on définit foundProducts comme l'article à trouver
+					(item) =>
+						item.idSelectedProduct === product.idSelectedProduct &&
+						item.colorSelectedProduct === product.colorSelectedProduct	
+				); //si les produits du panier et les produits du LS n'ont pas même ID et même couleur
+					// il retournera undefined  
+				if (
+					foundProducts == undefined &&
+					colorOptions.value != "" &&			//si les consitions sont OK
+					getProductQuantity.value > 0 &&
+					getProductQuantity.value < 100
+				) {
+					product.quantity = getProductQuantity.value; //la quantité saisie est définie 
+					basketValue.push(product);					 //dans le Ls
+				} else if (product.quantity >= 100){
+					alert("la quantité maximale est limité à 100 articles");
+					document.getElementById("addToCart").disabled = true;
+				}
+				else {
+					let newQuantity =
+						parseInt(foundProducts.quantity) +
+						parseInt(getProductQuantity.value); //CUMUL Quantité si présent ID et color
+					foundProducts.quantity = newQuantity;
+				}
+				saveBasket(basketValue);
+				alert(
+					`Le canapé ${nameKanap} ${colorOptions.value} a été ajouté en ${getProductQuantity.value} exemplaires à votre panier !`
+				);
+			}
+			//je crée une fonction de sauvegarde du panier
+			function saveBasket(basketValue) {
+				localStorage.setItem("kanapLs", JSON.stringify(basketValue));
+			}
 
+			// Si le choix de couleur est vide
+			if (colorOptions.value === "") {
+				alert("Veuillez choisir une couleur avant de valider");
+				document.getElementById("addToCart").disabled = true;
+			}
+			// Si la quantité choisie est nulle OU si elle dépasse 100
+			else if (
+				getProductQuantity.value <= 0 ||
+				getProductQuantity.value > 100
+			) {
+				alert("Veuillez sélectionner une quantité correcte");
+			} else {
+				//Si tout est OK, on envoie le panier au LS
+				addBasket(basketValue);
+			}
+		});
+	})
+    .catch(function (err) {
+		console.log(err);
+	});
+
+    let valeurActuelle = getProductQuantity.value;
+	if (valeurActuelle > 1 && valeurActuelle < 100) {
+		alert(`vous avez déjà ${valeurActuelle} article(s) dans votre panier, 
+				il est limité à 100 articles ! 
+				`);
+	} else if (valeurActuelle >= 100){
+		alert('Le panier à atteint sa limite maximale');
+		document.getElementById("addToCart").disabled = true;
+	}
