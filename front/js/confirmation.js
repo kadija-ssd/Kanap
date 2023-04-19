ef081345ff66891c240be2dd4b12e4e08a962cfd
kadija-ssd <kadija.sid@outlook.fr>
2023-04-19 11:29:01 +0200

    modif

--- front/js/confirmation.js
+++ front/js/confirmation.js
@@ -1,8 +1,8 @@
 // Récupère l'ordreId de l'URL
 let orderId = new URLSearchParams(window.location.search).get('orderId');
 
-// Met le N° de commande dans la zone HTML réservé à l'affichage
+// Met le numéro de commande dans la zone HTML réservé à l'affichage
 document.getElementById('orderId').textContent = orderId;
 
-// Vide le localStorage
+// Vide le localStorage à la suite de la commande
 window.localStorage.clear();
